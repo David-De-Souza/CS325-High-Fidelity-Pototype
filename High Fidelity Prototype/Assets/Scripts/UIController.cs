1a6652256497a1efc062444266644add67ef0aa8
David Desouza <70505948+David-De-Souza@users.noreply.github.com>
2021-11-26 18:58:08 +0800

    push - final product

--- High Fidelity Prototype/Assets/Scripts/UIController.cs
+++ High Fidelity Prototype/Assets/Scripts/UIController.cs
@@ -1,189 +1,187 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class UIController : MonoBehaviour
-{
-  public GameObject MainScreen;
-  public GameObject Library;
-  public GameObject Geography;
-  public GameObject Saved;
-  public GameObject DownloadButton;
-  public Text PageCounter;
-  public Text Description;
-  public Image CurrentModel;
-  public Button LibraryButton;
-
-  public Sprite[] Models;
-  public Sprite BackSprite;
-  public Sprite LibrarySprite;
-
-  bool IsLocalSimulation = false;
-
-  public int page_count;
-  int page_number = 1;
-
-  // Start is called before the first frame update
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Escape))
+    public class UIController : MonoBehaviour
     {
-      Debug.Log("Esc");
-      Application.Quit();
-    }
-  }
+    public GameObject MainScreen;
+    public GameObject Library;
+    public GameObject Geography;
+    public GameObject Saved;
+    public GameObject DownloadButton;
+    public Text PageCounter;
+    public Text Description;
+    public Renderer CurrentModel;
+    public Button LibraryButton;
 
-  void ChangeModel()
-  {
-    if (page_number == 1)
+    public Texture[] Models;
+    public Sprite BackSprite;
+    public Sprite LibrarySprite;
+
+    bool IsLocalSimulation = false;
+
+    public int page_count;
+    int page_number = 1;
+
+    // Start is called before the first frame update
+    void Start()
     {
-      CurrentModel.sprite = Models[0];
-      Description.text = "An oxbow lake starts out as a curve, or meander, in a river.\nA lake forms as the river finds a different, shorter, course.\nThe meander becomes an oxbow lake along the side of the river.\n\nOxbow lakes usually form in flat, low - lying plains close to\nwhere the river empties into another body of water.\nOn these plains, rivers often have wide meanders.";
+
     }
-    else if (page_number == 2)
+
+    // Update is called once per frame
+    void Update()
     {
-      CurrentModel.sprite = Models[1];
-      Description.text = "During a flood, the rapid flow of high water\nmight cut a new channel across the meander.";
+        ChangeModel();
     }
-    else if (page_number == 3)
+
+    void ChangeModel()
     {
-      CurrentModel.sprite = Models[2];
-      Description.text = "As sediment is deposited along the edges of the\nnew channel, the former meander might be\nisolated from the river and form an oxbow lake.";
+        if (page_number == 1)
+        {
+            CurrentModel.material.mainTexture = Models[0];
+            Description.text = "An oxbow lake starts out as a curve, or meander, in a river.\nA lake forms as the river finds a different, shorter, course.\nThe meander becomes an oxbow lake along the side of the river.\n\nOxbow lakes usually form in flat, low - lying plains close to\nwhere the river empties into another body of water.\nOn these plains, rivers often have wide meanders.";
+        }
+        else if (page_number == 2)
+        {
+            CurrentModel.material.mainTexture = Models[1];
+            Description.text = "During a flood, the rapid flow of high water\nmight cut a new channel across the meander.";
+        }
+        else if (page_number == 3)
+        {
+            CurrentModel.material.mainTexture = Models[2];
+            Description.text = "As sediment is deposited along the edges of the\nnew channel, the former meander might be\nisolated from the river and form an oxbow lake.";
+        }
     }
-  }
 
-  public void ToLibrary()
-  {
-    //if (IsLocalSimulation)
-    //{
-    //  MainScreen.SetActive(false);
-    //  Geography.SetActive(true);
-    //}
-    //else
-    //{
-    //  MainScreen.SetActive(false);
-    //  Library.SetActive(true);
-    //}
-    ScrnTransition.inst.FadeOut();
-    ScrnTransition.inst.EnqueueEvent(ToLibraryEvent());
-    ScrnTransition.inst.FadeIn();
-  }
-  IEnumerator ToLibraryEvent()
-  {
-    if (IsLocalSimulation)
+    public void ToLibrary()
     {
-      MainScreen.SetActive(false);
-      Geography.SetActive(true);
+        ScrnTransition.inst.FadeOut();
+        ScrnTransition.inst.EnqueueEvent(ToLibraryEvent());
+        ScrnTransition.inst.FadeIn();
     }
-    else
+    IEnumerator ToLibraryEvent()
     {
-      MainScreen.SetActive(false);
-      Library.SetActive(true);
+        if (IsLocalSimulation)
+        {
+            MainScreen.SetActive(false);
+            Geography.SetActive(true);
+        }
+        else
+        {
+            MainScreen.SetActive(false);
+            Library.SetActive(true);
+        }
+        yield return null;
     }
-    yield return null;
-  }
 
-  public void ToGeography()
-  {
-    //Library.SetActive(false);
-    //Geography.SetActive(true);
-    ScrnTransition.inst.FadeOut();
-    ScrnTransition.inst.EnqueueEvent(ToGeographyEvent());
-    ScrnTransition.inst.FadeIn();
-  }
-  IEnumerator ToGeographyEvent()
-  {
-    Library.SetActive(false);
-    Geography.SetActive(true);
-    yield return null;
-  }
+    public void ToGeography()
+    {
+        ScrnTransition.inst.FadeOut();
+        ScrnTransition.inst.EnqueueEvent(ToGeographyEvent());
+        ScrnTransition.inst.FadeIn();
+    }
+    IEnumerator ToGeographyEvent()
+    {
+        Library.SetActive(false);
+        Geography.SetActive(true);
+        yield return null;
+    }
 
-  public void Next()
-  {
-    if (page_number == page_count)
-      page_number = 1;
-    else
-      ++page_number;
+    public void Next()
+    {
+        if (page_number == page_count)
+            page_number = 1;
+        else
+            ++page_number;
 
-    PageCounter.text = page_number + "/3";
-    ChangeModel();
-  }
+        PageCounter.text = page_number + "/3";
+    }
 
-  public void Back()
-  {
-    if (page_number == 1)
-      page_number = page_count;
-    else
-      --page_number;
+    public void Back()
+    {
+        if (page_number == 1)
+            page_number = page_count;
+        else
+            --page_number;
 
-    PageCounter.text = page_number + "/3";
-    ChangeModel();
-  }
+        PageCounter.text = page_number + "/3";
+    }
 
-  public void ToMainFromLibrary()
-  {
-    //IsLocalSimulation = false;
-    //LibraryButton.image.sprite = LibrarySprite;
-    //Library.SetActive(false);
-    //DownloadButton.SetActive(true);
-    //MainScreen.SetActive(true);
-    ScrnTransition.inst.FadeOut();
-    ScrnTransition.inst.EnqueueEvent(ToMainFromLibraryEvent());
-    ScrnTransition.inst.FadeIn();
-  }
-  IEnumerator ToMainFromLibraryEvent()
-  {
-    IsLocalSimulation = false;
-    LibraryButton.image.sprite = LibrarySprite;
-    Library.SetActive(false);
-    DownloadButton.SetActive(true);
-    MainScreen.SetActive(true);
-    yield return null;
-  }
+    public void ToMainFromLibrary()
+    {
+        ScrnTransition.inst.FadeOut();
+        ScrnTransition.inst.EnqueueEvent(ToMainFromLibraryEvent());
+        ScrnTransition.inst.FadeIn();
+    }
+    IEnumerator ToMainFromLibraryEvent()
+    {
+        IsLocalSimulation = false;
+        LibraryButton.image.sprite = LibrarySprite;
+        Library.SetActive(false);
+        DownloadButton.SetActive(true);
+        MainScreen.SetActive(true);
+        yield return null;
+    }
 
-  public void ToLibraryFromGeography()
-  {
-    Geography.SetActive(false);
-    Library.SetActive(true);
-  }
+    public void ToLibraryFromGeography()
+    {
+        ScrnTransition.inst.FadeOut();
+        ScrnTransition.inst.EnqueueEvent(ToLibraryFromGeographyEvent());
+        ScrnTransition.inst.FadeIn();
+    }
 
-  public void Download()
-  {
-    StartCoroutine("SavedToLibrary");
-  }
+    IEnumerator ToLibraryFromGeographyEvent()
+    {
+        Geography.SetActive(false);
+        Library.SetActive(true);
+        yield return null;
+    }
 
-  IEnumerator SavedToLibrary()
-  {
-    Saved.SetActive(true);
-    yield return new WaitForSeconds(1.0f);
-    Saved.SetActive(false);
-  }
+    public void Download()
+    {
+        StartCoroutine("SavedToLibrary");
+    }
 
-  public void ToMainFromGeography()
-  {
-    //Geography.SetActive(false);
-    //DownloadButton.SetActive(false);
-    //MainScreen.SetActive(true);
-    //IsLocalSimulation = true;
-    //LibraryButton.image.sprite = BackSprite;
-    ScrnTransition.inst.FadeOut();
-    ScrnTransition.inst.EnqueueEvent(ToMainFromGeographyEvent());
-    ScrnTransition.inst.FadeIn();
-  }
-  IEnumerator ToMainFromGeographyEvent()
-  {
-    Geography.SetActive(false);
-    DownloadButton.SetActive(false);
-    MainScreen.SetActive(true);
-    IsLocalSimulation = true;
-    LibraryButton.image.sprite = BackSprite;
-    yield return null;
-  }
+    IEnumerator SavedToLibrary()
+    {
+        Saved.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        Saved.SetActive(false);
+    }
+
+    public void ToMainFromGeography()
+    {
+        ScrnTransition.inst.FadeOut();
+        ScrnTransition.inst.EnqueueEvent(ToMainFromGeographyEvent());
+        ScrnTransition.inst.FadeIn();
+    }
+    IEnumerator ToMainFromGeographyEvent()
+    {
+        Geography.SetActive(false);
+        DownloadButton.SetActive(false);
+        MainScreen.SetActive(true);
+        IsLocalSimulation = true;
+        LibraryButton.image.sprite = BackSprite;
+        yield return null;
+    }
+
+    public void DropdownScreen1()
+    {
+        page_number = 1;
+    }
+    public void DropdownScreen2()
+    {
+        page_number = 2;
+    }
+    public void DropdownScreen3()
+    {
+        page_number = 3;
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
 }
